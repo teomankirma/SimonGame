465399b4b7a1fd890f1d8fa3c1be8735003b0e22
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-11-23 14:15:22 -0800

    feat: generating random colors

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
-alert("Hello");
+const buttonColors = ["red", "blue", "green", "yellow"];
+const gamePattern = [];
+
+function nextSequence() {
+  const randomNumber = Math.floor(Math.random() * 4);
+  return randomNumber;
+}
+
+let randomChosenColor = buttonColors[nextSequence()];
+gamePattern.push(randomChosenColor);
