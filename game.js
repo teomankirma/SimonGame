7d27c92980d08a00aff750ba69c34a653ff04dd2
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-11-23 15:29:24 -0800

    feat: final version of the game

--- game.js
+++ game.js
@@ -1,10 +1,83 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
-const gamePattern = [];
+let gamePattern = [];
+let userClickedPattern = [];
+let started = false;
+let level = 0;
 
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text(`Level ${level}`);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function () {
+  const userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+// Function for generating random numbers
 function nextSequence() {
+  level++;
+  $("#level-title").text(`Level ${level}`);
+
   const randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColor);
 }
 
-let randomChosenColor = buttonColors[nextSequence()];
-gamePattern.push(randomChosenColor);
+// Function for playing sounds
+function playSound(name) {
+  const sound = new Audio(`sounds/${name}.mp3`);
+  sound.play();
+}
+
+// Function for animating the button
+function animatePress(currentColor) {
+  $(`#${currentColor}`).addClass("pressed");
+
+  setTimeout(function () {
+    $(`#${currentColor}`).removeClass("pressed");
+  }, 100);
+}
+
+// Function for checking the answer
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+        userClickedPattern = [];
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
+}
+
+// Function for restarting the game
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
